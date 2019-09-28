9693d002ea39623fd7ce6e8c4188e32b7e77390f
tje3d <tje3d@yahoo.com>
2019-09-28 22:55:41 +0330

    Ignore this

--- client/src/main.ts
+++ client/src/main.ts
@@ -15,6 +15,7 @@ Vue.component('l-map', LMap);
 Vue.component('l-tile-layer', LTileLayer);
 Vue.component('l-marker', LMarker);
 
+// @ts-ignore
 delete Icon.Default.prototype._getIconUrl;
 
 Icon.Default.mergeOptions({
